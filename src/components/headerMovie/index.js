db84518aa8f6ade2785b0d8354f093ca056685ef
Dean Crowley <deancrowley1@gmail.com>
2022-10-14 22:13:10 +0100

    Exercise - Show favourite icon on Movie Details page.

--- src/components/headerMovie/index.js
+++ src/components/headerMovie/index.js
@@ -5,9 +5,12 @@ import Paper from "@mui/material/Paper";
 import IconButton from "@mui/material/IconButton";
 import Typography from "@mui/material/Typography";
 import HomeIcon from "@mui/icons-material/Home";
+import Avatar from "@mui/material/Avatar";
+import FavoriteIcon from "@mui/icons-material/Favorite";
 
 const MovieHeader = (props) => {
   const movie = props.movie;
+  const favouriteMovies = JSON.parse(localStorage.getItem("favourites")); 
 
   return (
     <Paper 
@@ -23,6 +26,16 @@ const MovieHeader = (props) => {
       <IconButton aria-label="go back">
         <ArrowBackIcon color="primary" fontSize="large" />
       </IconButton>
+
+        {/* If the ID of a movie in favouriteMovie matches the ID of a movie, display the red heart icon inside the avatar on the movie header */}
+        {favouriteMovies.find((favMovies) => favMovies.id === movie.id) ? ( 
+          <Avatar sx={{ backgroundColor: 'red' }}>   {/* code from https://mui.com/material-ui/react-avatar/#icon-avatars */}
+          <
+            FavoriteIcon
+          />
+        </Avatar>
+        ) : null }
+      
 
       <Typography variant="h4" component="h3">
         {movie.title}
