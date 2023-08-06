2bdc85844ab15686900490dfe64c39995f89a114
Dean Crowley <deancrowley1@gmail.com>
2023-08-06 19:45:23 +0100

    Fix upcoming movies page

--- src/pages/upcomingMoviesPage.js
+++ src/pages/upcomingMoviesPage.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import PageTemplate from "../components/templateMovieListPage";
 import { getUpcomingMovies } from "../api/tmdb-api";
+import AddToFavouritesIcon from '../components/cardIcons/addToFavourites'
 
 const UpcomingMoviesPage = (props) => {
   const [movies, setMovies] = useState([]);
@@ -26,8 +27,11 @@ const UpcomingMoviesPage = (props) => {
     <PageTemplate
       title="Discover Upcoming Movies"
       movies={movies}
-      selectFavourite={addToFavourites}
+      action={(movie) => {
+        return <AddToFavouritesIcon movie={movie} />
+      }}
     />
   );
+  
 };
 export default UpcomingMoviesPage;
